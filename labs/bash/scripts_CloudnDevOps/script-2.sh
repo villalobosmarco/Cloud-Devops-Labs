f9d70988c554a390eaf4425047ba6dbae41c1598
villalobosmarco <root@marco.localdomain>
2023-11-19 17:38:40 -0600

    cleanup

--- labs/bash/scripts_CloudnDevOps/script-2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ""
-echo "-----------------------------------------------------------------------------"
-read -p "Ingrese el path a consultar. Seguidamente se mostratrá su tamaño y contenido: " path
-
-echo ""
-echo "El tamaño de" $path "es de" $(ls -sd $path | grep -Eo '[0-9]') "btyes." && echo "El contenido de" $path "se detalla a continuación:" $(ls $path)
-echo ""
