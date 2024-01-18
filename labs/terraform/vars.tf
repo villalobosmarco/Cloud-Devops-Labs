2640aa7ad4a1dbbee8fe67c2b1f0203d225557e6
villalobosmarco <86538298+villalobosmarco@users.noreply.github.com>
2024-01-17 22:13:10 -0600

    Add files via upload

---
+++ labs/terraform/vars.tf
@@ -0,0 +1,11 @@
+    variable "aws_access_key" {
+        type = string # Research how to use SENSITIVE instead.
+        default = "AKIA5FTY6QNRVRYFCQM5"
+      
+    }
+
+    variable "aws_secret_key" {
+        type = string # Research how to use SENSITIVE instead.
+        default = "Z1YDkj+oWmcSfyYh9Qoau4kP3pacLPDlBsYT8aNB"
+      
+    }
