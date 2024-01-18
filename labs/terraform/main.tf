6a1976133b144ed922fffccc0153debb96de70d1
villalobosmarco <86538298+villalobosmarco@users.noreply.github.com>
2024-01-17 23:58:14 -0600

    Update main.tf

--- labs/terraform/main.tf
+++ labs/terraform/main.tf
@@ -1,23 +1,24 @@
-provider "aws" {
-    region = "us-east-1"
-  
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.32.1"
+    }
+  }
 }
 
 module "aws_s3_module" {
-    source = "hashicorp/aws"
+    source = "terraform-aws-modules/s3-bucket/aws"
 
     attach_public_policy = "false"
-    # bucket = "Lab_S3" // Generates an error if used.
+    bucket = "labs323213123" // Generates an error if used.
     
     tags = {
         name = "Lab"
     }
 }
 
-resource "aws_s3_bucket" "Lab_S3" {
-        
-    tags = {
-      Name = "Lab"
-    }
- 
+provider "aws" {
+    region = "us-east-1"
+  
 }
