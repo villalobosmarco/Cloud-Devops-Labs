6395a30dcc95978118bf9f987f57d2e8b2de0424
villalobosmarco <86538298+villalobosmarco@users.noreply.github.com>
2024-01-17 23:52:03 -0600

    Update main.tf

--- labs/terraform/main.tf
+++ labs/terraform/main.tf
@@ -3,6 +3,17 @@ provider "aws" {
   
 }
 
+module "aws_s3_module" {
+    source = "hashicorp/aws"
+
+    attach_public_policy = "false"
+    # bucket = "Lab_S3" // Generates an error if used.
+    
+    tags = {
+        name = "Lab"
+    }
+}
+
 resource "aws_s3_bucket" "Lab_S3" {
         
     tags = {
